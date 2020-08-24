370b689191401f732820315b93565027e0ccf80a
unknown <hucvbty@gmail.com>
2020-08-25 01:30:25 +0800

    update

--- GetFileVersion/GetFileVersion.cpp
+++ GetFileVersion/GetFileVersion.cpp
@@ -1,35 +1,96 @@
 ﻿#include "windows.h"
 #include "stdio.h"
 
-UINT64 WINAPI GetModuleVersion(WCHAR* pModulePath)
+DWORD gVersion = 0;
+
+VOID WINAPI GetModuleVersion(CHAR* pModulePath)
 {
 	DWORD				dwHandle;
 	DWORD				VersionInfoSize;
-	VS_FIXEDFILEINFO* pFixedFileInfo;
+	VS_FIXEDFILEINFO*	pFixedFileInfo = NULL;
 	LARGE_INTEGER		ModuleVersion;
 	UINT				Size;
 	VOID* pVersionData = NULL;
 
 	ModuleVersion.QuadPart = 0;
-	VersionInfoSize = GetFileVersionInfoSizeW(pModulePath, &dwHandle);
+	VersionInfoSize = GetFileVersionInfoSizeA(pModulePath, &dwHandle);
 	pVersionData = malloc(VersionInfoSize);
-	if (pVersionData == NULL) goto End;
 
-	if (!GetFileVersionInfoW(pModulePath, 0, VersionInfoSize, pVersionData)) goto End;
+	if (pVersionData == NULL)
+		return;
 
-	if (!VerQueryValueW(pVersionData, L"\\", (VOID**)&pFixedFileInfo, &Size)) goto End;
-	ModuleVersion.HighPart = pFixedFileInfo->dwProductVersionMS;
-	ModuleVersion.LowPart = pFixedFileInfo->dwProductVersionLS;
-End:
-	if (pVersionData != NULL) free(pVersionData);
-	printf("%llx", ModuleVersion.QuadPart);
-	return (ModuleVersion.QuadPart);
+	if (!GetFileVersionInfoA(pModulePath, 0, VersionInfoSize, pVersionData))
+		return;
+
+	if (!VerQueryValueW(pVersionData, L"\\", (VOID**)&pFixedFileInfo, &Size))
+		return;
+	 
+	if (pFixedFileInfo->dwSignature == 0xfeef04bd)
+	{
+		if (gVersion == ((pFixedFileInfo->dwFileVersionLS >> 0) & 0xffff))
+		{
+			printf("Version: %d.%d   %s\n",
+				(pFixedFileInfo->dwFileVersionLS >> 16) & 0xffff,
+				(pFixedFileInfo->dwFileVersionLS >> 0) & 0xffff,
+				pModulePath
+			);
+		}
+	}
+	return;
 }
 
-int main()
+VOID TraverDict(PCHAR StartFilePath)
 {
-	WCHAR FilePath[MAX_PATH] = L"C:\\windows\\system32\\kernel32.dll";
+	CHAR NewPath[MAX_PATH] = "";
+	WIN32_FIND_DATAA FindFileData;
+	HANDLE hFind;
 
-	GetModuleVersion(FilePath);
+	sprintf(NewPath, "%s\\*", StartFilePath);
+	hFind = FindFirstFileA(NewPath, &FindFileData);
+
+	if (hFind == INVALID_HANDLE_VALUE)
+	{
+		printf("Dict Not Find: %s\n", StartFilePath);
+		return;
+	}
+
+	while (FindNextFileA(hFind, &FindFileData) != 0)                         
+	{
+		if ( lstrcmpA(FindFileData.cFileName, ".") == 0 || 
+			 lstrcmpA(FindFileData.cFileName, "..") == 0 )       
+		{
+			continue;
+		}
+
+		if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0)  
+		{
+			ZeroMemory(NewPath, MAX_PATH);
+			sprintf(NewPath, "%s\\%s", StartFilePath, FindFileData.cFileName);
+			TraverDict(NewPath);                               
+		}
+
+		if (!strstr(FindFileData.cFileName,".mui")&&
+			(strstr(FindFileData.cFileName, ".exe") || strstr(FindFileData.cFileName, ".dll")))
+		{
+			ZeroMemory(NewPath, MAX_PATH);
+			sprintf(NewPath, "%s\\%s", StartFilePath, FindFileData.cFileName);
+			GetModuleVersion(NewPath);
+		}
+	}
+	FindClose(hFind);
+}
+
+int main(int argc,char *argv[])
+{
+	CHAR StartPath[MAX_PATH] = "";
+
+	if (argc != 3)
+	{
+		puts("path_name version");
+		return 0;
+	}
+
+	gVersion = atoi(argv[2]);
+	TraverDict(argv[1]);
 };
 
